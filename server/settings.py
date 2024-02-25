9d216767a6e85ff78e3bc88c3f80205bd934ed1e
Hussain <hussainrizvi486@gmail.com>
2024-02-26 02:42:52 +0500

    changes in settings

--- server/settings.py
+++ server/settings.py
@@ -62,10 +62,7 @@ MIDDLEWARE = [
 ]
 
 # Cors Allowed Origins
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",  # Add your React app's origin
-    # Add other allowed origins as needed
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = "server.urls"
 
@@ -161,7 +158,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 
