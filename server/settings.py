eae545c86037df3f1c17958fe4685a168a26716e
Hussain <hussainrizvi486@gmail.com>
2024-05-16 03:01:22 +0500

    production branch

--- server/settings.py
+++ server/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-k8lbmu6fcuk6$#j0^^shsui=*v3+d(5w!s_834$%%!_7#nsod$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["paytusker.com"]
 
 
 # Application definition
